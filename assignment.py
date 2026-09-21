08cb5fb93bf981b35a1745544cef8f843b016fd4
TG31 <tuguldur.b2016@orkhonschool.edu.mn>
2026-09-21 09:16:11 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,31 +1,41 @@
-# You can remove 'pass' if you written code in the function 
-
 # Exercise 1
 def count_characters(text):
-    # Write your code here
-    pass
+    return len(text)
+print(count_characters("Hello World"))
 
 # Exercise 2
 def remove_spaces(text):
-    # Write your code here
-    pass
+    return text.replace(" ", "")
+print(remove_spaces("Python is fun"))
 
 # Exercise 3
 def count_vowels(text):
-    # Write your code here
-    pass
+    count = 0
+    for i in range(0, len(text)):
+        if text[i] == "a" or text[i] == "e" or text[i] == "i" or text[i] == "o" or text[i] == "u":
+            count += 1
+    return count
+print(count_vowels("Python is amazing"))
 
 # Exercise 4
 def replace_vowels(text):
-    # Write your code here
-    pass
+    for vowel in "aeiouAEIOU":
+        text = text.replace(vowel, "*")
+    return text
+print(replace_vowels("Education"))
 
 # Exercise 5
 def count_words(text):
-    # Write your code here
-    pass
+    text = text.split()
+    return len(text)
+print(count_words("Python makes coding fun"))
 
 # Exercise 6
 def find_longest_word(text):
-    # Write your code here
-    pass
+    text = text.split()
+    longest = text[0]
+    for i in range(0, len(text)):
+        if len(longest) <= len(text[i]):
+            longest = text[i]
+    return longest
+print(find_longest_word("Learning Python programming"))
